664459447f6826d27d71eb795d8ac1f605796226
Junhyeock <dlwnsehd13@gmail.com>
2025-07-20 16:45:27 +0900

    Add files via upload

---
+++ task_side/mymath.py
@@ -0,0 +1,11 @@
+import math
+
+def triangle_area(base, height):
+    return 0.5 * base * height
+
+def circle_area(radius):
+    return math.pi * radius ** 2
+
+def rectangle_area(width, height):
+    return width * height
+
